a2a7541f1a68431d457911106d279630d2f0a75b
Bunyod2009 <bunyodshavkatov362@gmail.com>
2024-09-07 14:58:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var age = +prompt("Введите свой возраст")
+if (age <= 17) {
+    alert("Вы ещё молоды. Вам нужна учиться")
+} else if (age <= 18) {  
+    alert("Вам нужна работать")
+} else if (age <= 50) {
+    alert("Вам нужна работать")
+} else if (age <= 60) {
+    alert("Вам скоро на пенсию")
+} else if (age <= 80) {
+    alert("Вы пенсионер")
+} else {
+   alert("неправильная информация")
+}
+var oclock = +prompt("Введите время")
+if (oclock <= (10)) {
+    alert("Часов утра")
+} else if (oclock <= (11)) {
+    alert("Часов утра")
+} else if (oclock <= (12)) {
+    alert("Час дня")
+} else if (oclock <= (13)) {
+    alert("Обед")
+} else if (oclock <= (15)) {
+    alert("После обеда")
+} else if (oclock <= (17)) {
+    alert("Часов вечера")
+} else {
+   alert("неправильная время")
+}
+var raqam = +prompt("Введите первое число")
+var raqam = +prompt("Введите втарое число")
+var raqam = +prompt("Введите третие число")
+if (raqam <= (2 < 1 || 1 < 3)){
+}alert(`Cреднее число ${2}`)
